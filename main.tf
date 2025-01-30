d5c8b0db408f6ff81a4396d4dccb773ef36333c9
kaizenacademy <elea092019@gmail.com>
2025-01-30 20:42:27 +0000

    modified readmy file

--- main.tf
+++ main.tf
@@ -10,7 +10,12 @@ resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
   cidr_block = var.subnet_cidr
 
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
   tags = {
-    Name = "Main"
+    Name = var.igw_name
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,5 @@
 variable region {}
 variable vpc_cidr {}
 variable subnet_cidr {}
+variable igw_name {}
+  
